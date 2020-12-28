3a60d0c3069ece3882112ea600a9ecaa93ed8cfc
Leonel Velasquez <leonelvelasquez@gmail.com>
2020-12-27 22:19:35 -0600

    Added edit and update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,14 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         # render plain:@article.inspect
@@ -21,7 +29,14 @@ class ArticlesController < ApplicationController
         end
     end
 
-    def new
-        @article = Article.new
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end
+    
 end
