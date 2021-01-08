b6d9dc9d688f24dd9f0aa142c6ab3b5666d48e33
Leonel Velasquez <leonelvelasquez@gmail.com>
2021-01-08 00:07:52 -0600

    Create obj variable in shared/errors partial for generic use. Added obj in articles form and in user form, added signup route and user routes (signup for new action)

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # resources :articles, only: [:show, :index, :create, :new, :edit, :update, :destroy]
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
